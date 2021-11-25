1af89585a9e83f116c751b310698ce5191dac68c
mansiondebian <88039641+mansiondebian@users.noreply.github.com>
2021-11-25 00:40:26 -0300

    Update parametrizadoDSL.groovy

--- parametrizadoDSL.groovy
+++ parametrizadoDSL.groovy
@@ -19,22 +19,7 @@ job('ejemplo2-job-DSL') {
     	}
   	publishers {
       		mailer('sergioalvarez222@hotmail.com', true, true)
-      		slackNotifier {
-		  notifyAborted(true)
-		  notifyEveryFailure(true)
-		  notifyNotBuilt(false)
-		  notifyUnstable(false)
-		  notifyBackToNormal(true)
-		  notifySuccess(false)
-		  notifyRepeatedFailure(false)
-		  startNotification(false)
-		  includeTestSummary(false)
-		  includeCustomMessage(false)
-		  customMessage(null)
-		  sendAs(null)
-		  commitInfoChoice('NONE')
-		  teamDomain(null)
-		  authToken(null)
+      		
         	}
     	}
 }
